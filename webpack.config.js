514ab08621c5ea5f9d0ce48927f2b18dde3f525c
htoniv <vinothkumarrenganathan@users.noreply.github.com>
2018-01-15 11:14:32 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,52 @@
-{
-  "name": "kanbanboard",
-  "version": "1.0.0",
-  "description": "",
-  "main": "webpack.config.js",
-  "scripts": {
-    "dev": "./node_modules/.bin/webpack-dev-server --content-base --inline --hot",
-    "test": "echo \"Error: no test specified\" && exit 1"
+var webpack = require('webpack');
+var path = require('path');
+
+var loaders = [{
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  loader: "babel-loader"
+}, {
+  test: /\.css$/,
+  loader: "style-loader!css-loader"
+}, { 
+  test: /\.(png|jpg|woff2|eot|ttf|svg)$/, 
+  loader: 'url-loader?limit=100000' 
+},{ 
+  test: /\.jpeg$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/, 
+  loader: "file-loader"
+}];
+
+
+module.exports = {
+  entry: [
+    './dev/index.jsx'
+  ],
+  output: {
+    publicPath: './',
+    path: path.join(__dirname),
+    filename: 'app.min.js'
   },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/vinothkumarrenganathan/kanbanboard-rjs.git"
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-  "author": "vinoth",
-  "license": "ISC",
-  "bugs": {
-    "url": "https://github.com/vinothkumarrenganathan/kanbanboard-rjs/issues"
+  module: {
+    loaders
   },
-  "homepage": "https://github.com/vinothkumarrenganathan/kanbanboard-rjs#readme",
-  "dependencies": {
-    "antd": "^3.1.1",
-    "babel-runtime": "^6.26.0",
-    "body-parser": "^1.18.2",
-    "express": "^4.16.2",
-    "react": "^16.2.0",
-    "react-dom": "^16.2.0"
-  },
-  "devDependencies": {
-    "babel": "^6.23.0",
-    "babel-core": "^6.26.0",
-    "babel-loader": "^7.1.2",
-    "babel-plugin-import": "^1.6.3",
-    "babel-plugin-transform-class-properties": "^6.24.1",
-    "babel-plugin-transform-runtime": "^6.23.0",
-    "babel-preset-es2015": "^6.24.1",
-    "babel-preset-react": "^6.24.1",
-    "css-loader": "^0.28.7",
-    "file-loader": "^1.1.6",
-    "nodemon": "^1.14.11",
-    "style-loader": "^0.19.1",
-    "url-loader": "^0.6.2",
-    "webpack": "^3.10.0",
-    "webpack-dev-server": "^2.10.1"
-  },
-  "babel": {
-    "presets": [
-      "es2015",
-      "react"
-    ],
-    "plugins": [
-      [
-        "import",
-        {
-          "libraryName": "antd",
-          "style": "css"
-        }
-      ],
-      [
-        "transform-runtime",
-        {
-          "moduleName": "babel-runtime"
-        }
-      ]
-    ]
-  }
-}
+  watch: true,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        drop_console: false,
+        drop_debugger: true
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+  ]
+};
