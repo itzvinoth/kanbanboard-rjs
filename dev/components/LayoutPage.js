3eb69d3eae3e67205d3923a6ec8ffe4e854afc7c
htoniv <vinothkumarrenganathan@users.noreply.github.com>
2018-01-19 16:57:24 +0530

    Update LayoutPage.js

--- dev/components/LayoutPage.js
+++ dev/components/LayoutPage.js
@@ -32,19 +32,19 @@ export default class LayoutPage extends React.Component {
     return (
       <div style={{height:"100%", width:"1500px"}}>
         <Layout style={{minHeight:"100%"}}>
-          <Header style={{background:"#6666ff"}}>
-            <div style={{float:"left"}}><Icon type="select" style={{background:"#333300", color:"#FFFFFF", fontSize: "20px", margin:"10px", border:"8px solid black", borderRadius: "4px"}}/></div>
-            <div style={{ backgroundColor:"#6666ff"}}>
-              <Menu mode="horizontal" style={{ background:"#6666ff", color:"#FFFFFF", float:"left", border: "2px" }} >
+          <Header style={{background:"#1990ea"}}>
+            <div style={{float:"left"}}><Icon type="select" style={{background:"#1166a5", color:"#FFFFFF", fontSize: "20px", margin:"10px", border:"8px solid #1166a5", borderRadius: "4px"}}/></div>
+            <div style={{ backgroundColor:"#1990ea"}}>
+              <Menu mode="horizontal" style={{ background:"#1990ea", color:"#FFFFFF", float:"left", border: "2px" }} >
                 <Menu.Item key="1">Boards&nbsp;&nbsp;<span><Icon type="down" style={{color: "#FFFFFF", fontSize: "8px"}}/></span></Menu.Item>
-                <Menu.Item key="2">Activity<Badge count={2} style={{background: "#ff6600", color: "#FFFFFF", marginTop:"-20px"}}/></Menu.Item>
-                <Menu.Item key="3">Team mates</Menu.Item>
+                <Menu.Item key="2">Activity<Badge count={2} style={{background: "#ff7800", color: "#FFFFFF", marginTop:"-20px"}}/></Menu.Item>
+                <Menu.Item key="3">Teammates</Menu.Item>
               </Menu>
               <div style={{marginLeft: "90%"}}>
               <Dropdown overlay={menu} trigger={["click"]}>
                 <div style={{marginTop: "6%", textAlign:"center"}}>
                 <Avatar src={imageUrl} />
-                  <Icon type="down" style={{color: "#FFFFFF"}}/>
+                  <Icon type="down" style={{color: "#FFFFFF", position:"absolute", marginTop:"20px", marginLeft:"5px"}}/>
                 </div>
               </Dropdown>
               </div>
@@ -53,14 +53,14 @@ export default class LayoutPage extends React.Component {
           <Header style={{ width: "100%", background:"#FFFFFF" }}>
             <Row gutter={16}>
               <Col className="gutter-row" span={14}>
-                <h3 style={{color:"#000000", float:"left", marginLeft: "10px"}}>Design Backlog &nbsp;&nbsp;</h3>
-                <h3 style={{color:"#999999"}}>|&nbsp;&nbsp;Dashboard Design Team</h3>
+                <h3 style={{color:"#354052", float:"left", marginLeft: "10px", fontSize: "16px"}}>Design Backlog &nbsp;&nbsp;</h3>
+                <h3 style={{color:"#7f8fa4", fontSize: "12px", marginTop: "1.5px"}}>|&nbsp;&nbsp;Dashboard Design Team</h3>
               </Col>
               <Col className="gutter-row" span={7} style={{textAlign: "right"}}>
                 <Search placeholder="Search" style={{ width: "300px" }}/>
               </Col>
               <Col className="gutter-row" span={3} style={{textAlign: "left"}}>
-                <Button style={{ background:"#6666ff", color: "#FFFFFF" }}>
+                <Button style={{ background:"#1990ea", color: "#FFFFFF" }}>
                   <Icon type="plus" />New Card
                 </Button>
               </Col>
